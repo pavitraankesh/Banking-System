c2daedf0d65ddba0dc0b91ee06311e0ff1874e65
pavitraankesh <70202515+pavitraankesh@users.noreply.github.com>
2022-09-26 19:11:21 +0530

    updated

--- Code/Banking.java
+++ Code/Banking.java
@@ -1,466 +1,71 @@
-package Banking;
-import java.util.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.RandomAccessFile;
-import java.io.FileNotFoundException;
+//main function class
 
-public class Account 
+package Banking;
+import Banking.Account;
+import java.util.*;
+
+public class Banking 
 {
-	protected String name;
-	protected String phone_no;
-	protected String city;
-	protected String username;
-	protected long password;
-	protected long amount;
-	protected int account_ID;
-	
-	void createAccount()
+	public static void main(String[] args) 
 	{
-		System.out.println("\nCREATE ACCOUNT FORM: ");
+		System.out.println("********************************************************************");
+		System.out.println("*****MONEY BANK*****");
 		System.out.println("********************************************************************");
 		System.out.println();
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter name: ");
-		name = sc.nextLine();
-		System.out.println("Enter city : ");
-		city = sc.next();
-		
-		//to validate phone number
+		int choice1;
 		do
 		{
-			try
+			System.out.println("\n1. CREATE AN ACCOUNT \n2. LOGIN TO AN EXISTING ACCOUNT\n3. EXIT");
+			System.out.print("  ENTER YOUR CHOICE: ");
+			choice1=sc.nextInt();
+			switch(choice1)
 			{
-				System.out.println("Enter contact number : ");
-				phone_no = sc.next();
-				int flag=0;
-				int len = phone_no.length();
-				for(int i=0;i<len;i++)
-				{
-					char ch=phone_no.charAt(i);
-					if(!Character.isDigit(ch))
-					{
-						flag++;
-						break;
-					}
-					
-				}
-				if(flag==0&&len==10)
-				{
+				case 1: 
+					Account a=new Account();
+					a.createAccount();
 					break;
-				}
-				else
-					throw new Exception(" ");
-			}
-			catch(Exception e)
-			{
-				System.out.println("\tInvalid phone number");
-			}
-		}while(true);
-		
-		//username
-		do{
-			try{
-				System.out.println("Enter username : ");
-				username = sc.next();
-				int count=0;
-				String line="";
-				RandomAccessFile raf=new RandomAccessFile("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accounts.txt","rw");
-				while((line=raf.readLine())!=null)
-				{
-					String words[]=line.split(":");
-					if(words[4].equals(username))
-						count++;
-				}
-				raf.close();
-				if(count!=0)
-				{
-					throw new Exception(" ");
-				}
-				else
-					break;
-			}
-			catch(Exception e)
-			{
-				System.out.println("\tThe username is already taken.");
-			}
-		}while(true);
-		
-		//enter password
-		System.out.println("Set a password (only digits): ");
-		password = sc.nextLong();
-		
-		//to re-confirm password
-		do
-		{
-		try
-		{
-			System.out.println("Re-confirm password: ");
-			long temp_pass = sc.nextLong();
-			if(password == temp_pass) 
-			{
-				break; 
-			}
-			else
-			{
-				throw new Exception(" ");
-			}
-		}
-		catch(Exception e)
-		{
-			System.out.println("\tThe passwords don't match");
-        }
-		}while(true);
-		
-		//to enter minimum amount
-		do
-		{
-		try
-		{
-			System.out.println("Enter initial amount to deposited: ");
-			amount = sc.nextLong();
-			if(amount>10000) 
-				break; 
-			else
-				throw new Exception(" ");
-		}
-		catch(Exception e)
-		{
-			System.out.println("\tThe minimum amount must be Rs.10,000");
-        }
-		}while(true);
-		System.out.println();
-		saveAccount();
-	} //closing createAccount
-	
-	//generate account number
-	void generateAcNo()
-	{
-		try{
-			int countl=0;
-			RandomAccessFile raf=new RandomAccessFile("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accounts.txt","rw");
-			while((raf.readLine())!=null)
-			{
-				countl++;
-			}
-			account_ID=(++countl);
-			raf.close();
-		}
-		catch(FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-		
-	}
-	
-	//saving the account
-	void saveAccount()
-	{
-		try
-		{
-			FileWriter fw=new FileWriter("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accounts.txt",true);
-			PrintWriter pw=new PrintWriter(fw);
-			generateAcNo();
-			pw.print(account_ID); pw.print(":");
-			pw.print(name);pw.print(":");
-			pw.print(city);pw.print(":");
-			pw.print(phone_no);pw.print(":");
-			pw.print(username);pw.print(":");
-			pw.print(password);pw.print(":");
-			pw.println(amount);
-			fw.close();
-			System.out.println("ACCOUNT IS SUCCESSFULLY CREATED");
-			System.out.println("Your account number is: "+account_ID);
-			System.out.println("********************************************************************");
-			System.out.println();
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	//login
-	int login()
-	{
-		System.out.println("\nLOGIN FORM: ");
-		System.out.println("********************************************************************");
-		System.out.println();
-		Scanner sc=new Scanner(System.in);
-		try
-		{
-			System.out.print("Username: ");
-			username=sc.next();
-			String line="";
-			int flag=0;
-			RandomAccessFile raf=new RandomAccessFile("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accounts.txt","rw");
-			while((line=raf.readLine())!=null)
-			{
-				String words[]=line.split(":");
-				if(words[4].equals(username))
-					flag=1;
-			}
-			raf.close();
-			if(flag==0)
-			{
-				throw new Exception(" ");
-			}
-		}
-		catch(Exception e)
-		{
-			System.out.println("\tUsername does not exist. ");
-			return 0;
-		}
-		
-		//enter password
-		do
-		{
-			try
-			{
-				System.out.print("Password: ");
-				password=sc.nextLong();
-				String line="";
-				RandomAccessFile raf=new RandomAccessFile("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accounts.txt","rw");
-				while((line=raf.readLine())!=null)
-				{
-					String words[]=line.split(":");
-					if(words[4].equals(username))
+				case 2:
+					Account ac=new Account();
+					int res=ac.login();
+					while(res!=0)
 					{
-						if(Long.toString(password).equals(words[5]))
+						int choice2; int log=0;
+						System.out.println("\n1. DEPOSIT \n2. WITHDRAW\n3. TRANSFER\n4. CHECK BALANCE\n5. LOGOUT");
+						System.out.print("  ENTER YOUR CHOICE: ");
+						choice2=sc.nextInt();
+						Account acc=new Account();
+						switch(choice2)
 						{
-							System.out.println("\nLOGIN SUCCESSFUL!!");
-							System.out.println("Welcome "+words[1]+"!");
-							return Integer.parseInt(words[0]);
+							case 1: 
+								acc.deposit(res);
+								break;
+							case 2:
+								acc.withdraw(res);
+								break;
+							case 3:
+								acc.transfer(res);
+								break;
+							case 4:
+								acc.check_balance(res);
+								break;
+							case 5:
+								acc.logout(res);
+								log=1;
+								break;
+							default:
+								System.out.println("Invalid Entry.");
 						}
-						else
-							throw new Exception(" ");
+						if(log==1)
+							break;
 					}
-				}
-			}
-			catch(Exception e)
-			{
-				System.out.println("\tIncorrect password.");
+					break;
+				case 3:
+					System.out.println("Thank you for choosing Money Bank. Visit again.");
+					System.exit(0);
+				default:
+					System.out.println("Invalid Entry.");
 			}
 		}while(true);
-	}
-	
-	void update(File f, File ftemp)
-	{
-		try {
-		String nline="";
-		RandomAccessFile raf=new RandomAccessFile(ftemp,"rw");
-		FileWriter fw=new FileWriter(f,false);
-		PrintWriter pw=new PrintWriter(fw);
-		while((nline=raf.readLine())!=null)
-		{
-			pw.println(nline);
-		}
-		raf.close();
-		pw.close();
-		}
-		catch(FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	int compute(int acc_no, long amt, int op)
-	{
-		File f=new File("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accounts.txt");
-		File ftemp=new File("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accountstemp.txt");
-		try {
-		String line="";
-		RandomAccessFile raf=new RandomAccessFile(f,"rw");
-		FileWriter fw=new FileWriter(ftemp,false);
-		PrintWriter pw=new PrintWriter(fw);
-		while((line=raf.readLine())!=null)
-		{
-			String words[]=line.split(":");
-			if(Integer.toString(acc_no).equals(words[0]))
-			{
-				if(op==1)
-				{
-					amt=amt+Long.parseLong(words[6]);
-				}
-				else
-				{
-					amt=Long.parseLong(words[6])-amt;
-					if(amt<10000)
-					{
-						throw new Exception(" ");
-					}
-				}
-				for(int i=0;i<6;i++)
-				{
-					pw.print(words[i]);
-					pw.print(":");
-				}
-				pw.println(amt);
-			}
-			else
-			{
-				pw.println(line);
-			}
-		}
-		raf.close();
-		fw.close();
-		update(f,ftemp);
-		return 1;
-		}
-		catch(FileNotFoundException e)
-		{
-			e.printStackTrace();
-			return 0;
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-			return 0;
-		}
-		catch(Exception e)
-		{
-			System.out.println("Minimum balance of Rs.10,000 must be maintained.\n");
-			return 0;
-		}
-	}
-	
-	//depositing
-	void deposit(int acc_no)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("\nDEPOSIT FORM: ");
-		System.out.println("********************************************************************");
-		System.out.println();
-		System.out.println("Account Number: "+acc_no);
-		System.out.println();
-		System.out.println("Enter the amount to be deposited: ");
-		long amt=sc.nextLong();
-		int res=compute(acc_no,amt,1);
-		if(res==1)
-			System.out.println("The amount has been succesfully deposited. ");
-		
-	}
-	
-	void withdraw(int acc_no)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("\nWITHDRAWAL FORM: ");
-		System.out.println("********************************************************************");
-		System.out.println();
-		System.out.println("Account Number: "+acc_no);
-		System.out.println();
-		System.out.println("Enter the amount to be withdrawn: ");
-		long amt=sc.nextLong();
-		int res=compute(acc_no,amt,0);
-		if(res==1)
-			System.out.println("The amount has been successfully withdrawn.");
-	}
-	
-	void transfer(int acc_no)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("\nTRANSFER FORM: ");
-		System.out.println("********************************************************************");
-		System.out.println();
-		System.out.println("Account Number: "+acc_no);
-		System.out.println();
-		int tan;
-		try 
-		{
-			System.out.print("Transfer to Account Number: ");
-			tan=sc.nextInt();
-			String line="";
-			int flag=0;
-			RandomAccessFile raf=new RandomAccessFile("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accounts.txt","rw");
-			while((line=raf.readLine())!=null)
-			{
-				String words[]=line.split(":");
-				if(tan==Integer.parseInt(words[0])) {
-					flag=1;
-					break; }
-	
-			}
-			raf.close();
-			if(flag==0)
-			{
-				throw new Exception(" ");
-			}
-		}
-		catch(Exception e)
-		{
-			System.out.println("\tAccount number does not exist. ");
-			return;
-		}
-		System.out.print("Enter the amount to be transferred: ");
-		long tamt=sc.nextLong();
-		int res1=compute(acc_no,tamt,0);
-		if(res1==0)
-		{
-			System.out.println("Transfer is unsuccessful.");
-		}
-		else
-		{
-			int res2=compute(tan,tamt,1);
-			if(res2==1)
-			{
-				System.out.println("The money has been successfully transferred.");
-			}
-		}
-	}
-	
-	//check balance
-	void check_balance(int acc_no)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("\nCHECK BALANCE FORM: ");
-		System.out.println("********************************************************************");
-		System.out.println();
-		File f=new File("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accounts.txt");
-		File ftemp=new File("C:\\Users\\Pavitra\\eclipse-workspace\\Project- Banking System\\src\\Banking\\accountstemp.txt");
-		try
-		{
-		    String line="";
-		    RandomAccessFile raf=new RandomAccessFile(f,"rw");
-		    FileWriter fw=new FileWriter(ftemp,false);
-		    PrintWriter pw=new PrintWriter(fw);
-		    while((line=raf.readLine())!=null)
-		    {
-			    String words[]=line.split(":");
-			    if(Integer.toString(acc_no).equals(words[0]))
-			    {
-			    	System.out.println("Account number:\t"+words[0]+"\nName:\t\t"+words[1]+"\nUsername:\t"+words[4]+"\nCity:\t\t"+words[2]+"\nPhone number:\t"+words[3]+"\nBalance:\t"+words[6]);
-			    	System.out.println("********************************************************************");
-			    	System.out.println();
-			    }
-			}
-		}
-		catch(FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	//logout
-	void logout(int acc_no)
-	{
-		System.out.println();
-		System.out.println("Account number: "+acc_no);
-		System.out.println("LOGGED OUT SUCCESFULLY!!");
-		System.out.println("********************************************************************");
-		System.out.println();
 	}
 }
-
